8078fb9b0605c91476e230091a656deae0083f13
Carlos Chacin <Hermitter@jellyapple.com>
2023-05-19 00:24:32 -0400

    Define the original themes seperate from oh-lucy

--- src/index.mjs
+++ src/index.mjs
@@ -1,57 +1,84 @@
 import { writeFile } from 'fs'
 import { promisify } from 'util'
 import chroma from 'chroma-js'
-import getTheme from './getTheme.mjs'
-import colors from './colors.mjs'
+// Original Lucy
+import og_theme from './original/theme.mjs'
+import og_colors from './original/colors.mjs'
+// Oh Lucy
+import theme from './custom/theme.mjs'
+import colors from './custom/colors.mjs'
 
-const promisifiedWriteFile = promisify(writeFile)
+const promisifiedWriteFile = promisify(writeFile);
 
-// These variants need to be listed as seperate themes in the package.json
-const VARIANTS = {
-  lucy: (color) => color,
-  'lucy-evening': (color) => {
-    const [red, green, blue, alpha] = chroma(color).rgba()
+const calculate_evening_theme = (color) => {
+  const [red, green, blue, alpha] = chroma(color).rgba();
 
-    const sum = red + green + blue
+  const sum = red + green + blue;
 
-    const clamp = (number) => Math.min(Math.max(number, 0), 255)
+  const clamp = (number) => Math.min(Math.max(number, 0), 255);
 
-    // Shift colors while preserving luminosity
-    const newRed = clamp(red * (1 + 0.175 * (1 - sum / 800)))
-    const newGreen = clamp(green * (1 - 0.01 * (1 - sum / 800)))
-    const newBlue = clamp(sum - (newRed + newGreen))
+  // Shift colors while preserving luminosity
+  const newRed = clamp(red * (1 + 0.175 * (1 - sum / 800)));
+  const newGreen = clamp(green * (1 - 0.01 * (1 - sum / 800)));
+  const newBlue = clamp(sum - (newRed + newGreen));
 
-    return chroma({ r: newRed, g: newGreen, b: newBlue, a: alpha }).hex()
-  }
+  return chroma({ r: newRed, g: newGreen, b: newBlue, a: alpha }).hex();
 }
 
-const getExportPath = (name) => `./dist/${name}.json`
+/* REMINDER: Variants need to be listed as seperate themes in the package.json */
+const VARIANTS = {
+  // Original Lucy themes
+  'lucy': {
+    theme: og_theme,
+    colors: og_colors,
+    getColor: (color) => color,
+  },
+  'lucy-evening': {
+    theme: og_theme,
+    colors: og_colors,
+    getColor: (color) => calculate_evening_theme(color),
+  },
+  // Modified Lucy themes
+  'oh-lucy': {
+    theme: og_theme,
+    colors: og_colors,
+    getColor: (color) => color,
+  },
+  'oh-lucy-evening': {
+    theme: og_theme,
+    colors: og_colors,
+    getColor: (color) => calculate_evening_theme(color),
+  },
+};
 
-const buildTheme = async () => {
+const buildTheme = async (varients) => {
   try {
     await Promise.all(
-      Object.entries(VARIANTS).map(([variantName, getColor]) => {
-        const themeWithColors = getTheme({
-          name: variantName,
-          colors: Object.entries(colors).reduce(
+      // For each theme varient
+      Object.entries(varients).map(([variantName, varient]) => {
+        // Assemble the theme's JSON
+        const themeWithColors = varient.theme({
+          'name': variantName,
+          'colors': Object.entries(varient.colors).reduce(
             (acc, [colorName, colorValue]) => ({
               ...acc,
-              [colorName]: getColor(colorValue)
+              [colorName]: varient.getColor(colorValue)
             }),
             {}
           )
-        })
+        });
 
+        // Export it to a file
         return promisifiedWriteFile(
-          getExportPath(variantName),
+          `./dist/${variantName}.json`,
           JSON.stringify(themeWithColors)
-        )
+        );
       })
-    )
-    console.log('🌺 Theme built. 💅')
+    );
+    console.log('🌺 Theme built. 💅');
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-buildTheme()
+buildTheme(VARIANTS)
